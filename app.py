d4f1cb29bcad5d435d34657c3ae84fe7b69de030
sanskarjoshi0607 <sanskarjoshi0607@gmail.com>
2026-06-22 12:36:57 +0530

    Added overs and balls input

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 
+
 teams = ['Sunrisers Hyderabad',
  'Mumbai Indians',
  'Royal Challengers Bangalore',
@@ -25,7 +26,7 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
 pipe = pickle.load(open('pipe.pkl','rb'))
-st.title('IPL Win Predictor')
+st.title('🏏 IPL Win Predictor')
 
 col1, col2 = st.columns(2)
 
@@ -51,12 +52,23 @@ with col3:
         step=1
     )
 with col4:
-    overs = st.number_input(
-        'Overs completed',
-        min_value=0.0,
-        max_value=20.0,
-        step=0.1
-    )
+    over_col1, over_col2 = st.columns(2)
+
+    with over_col1:
+        overs = st.number_input(
+            "Overs",
+            min_value=0,
+            max_value=20,
+            step=1
+        )
+
+    with over_col2:
+        balls = st.number_input(
+            "Balls",
+            min_value=0,
+            max_value=5,
+            step=1
+        )
 with col5:
     wickets = st.number_input(
         'Wickets out',
@@ -68,9 +80,11 @@ with col5:
 
 if st.button('Predict Probability'):
     runs_left = target - score
-    balls_left = 120 - (overs*6)
+    balls_bowled = overs * 6 + balls
+    balls_left = 120 - balls_bowled
+    overs_completed = overs + (balls / 6)
     wickets = 10 - wickets
-    crr = score / overs if overs > 0 else 0
+    crr = score / overs_completed if overs_completed > 0 else 0
     rrr = (runs_left * 6) / balls_left if balls_left > 0 else 0
 
     input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
